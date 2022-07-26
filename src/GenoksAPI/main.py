08caf7cab9cb5b2de42fb0c2018d0d6fca4c521b
Masoud Edizadeh <94247256+masoudedi@users.noreply.github.com>
2022-07-26 14:42:03 +0300

    Update main.py

--- src/GenoksAPI/main.py
+++ src/GenoksAPI/main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 class Genoks_api:
     def __init__(self):
@@ -12,9 +13,9 @@ class Genoks_api:
         r = requests.get(self.api_urls[endpoint])
         return r.json()
 
-    def create(self, endpoint, dataJson):
+    def Post(self, endpoint, dataJson):
         r = requests.post(self.api_urls[endpoint],
-        data=dataJson,
+        data=json.dumps(dataJson),
         headers={
             'Content-Type': 'application/json'
         })
@@ -26,8 +27,24 @@ class Genoks_api:
         headers={
         'Content-Type': 'application/json'
         })
+class Post_sample:
+    pass
 
 if __name__ == "__main__":
     api = Genoks_api()
-    samples = api.Get("variants")
-    print(samples)
+    samples = api.Get("samples")
+    data={
+    "data": {
+        "sample_id": "842-G-AlUmBar",
+        "patient_name": "Alias Umu Barbara",
+        "result_date": "2022-07-26T11:15:36.559Z",
+        "save_date": "2022-07-26T11:15:36.559Z",
+        "sequencing_technique": "WES",
+        "sequencing_type": "NovaSeq",
+        "customer_id": 1,
+        "status": 0,
+        "run_id": "123456789"
+    }
+    }
+    posSample = api.Post("samples", data)
+    print(posSample)
